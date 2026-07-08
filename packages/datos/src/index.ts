5bbb5071d5c6ff60b5943d295f673d92afa840a0
Sergio Lacaci <juersio@gmail.com>
2026-07-08 17:08:31 +0200

    Add padron connector parsing CKAN excel files

--- packages/datos/src/index.ts
+++ packages/datos/src/index.ts
@@ -1,2 +1,10 @@
 export { fetchPopulation, type PopulationSeries, type PopulationYear } from "./ine";
 export { fetchCkanDataset, type CkanDataset, type CkanResource } from "./ckan";
+export {
+  fetchPadronData,
+  type PadronData,
+  type AgeGroup,
+  type DistrictPopulation,
+  type SectionPopulation,
+  type BirthsDeathsYearly,
+} from "./padron";
